39839d7d86f81e26e6a18979d9e27994849afadb
igorcosta12 <igorcostasilva50@gmail.com>
2026-03-06 11:03:33 -0300

    Correções

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function RootLayout() {
@@ -10,49 +10,49 @@ export default function RootLayout() {
   function limpar() {
     setDisplay("");
   }
-  function calcular() {
+ function calcular() {
     const resultado = eval(display);
-    setDisplay(String(resultado));
-  }
+    setDisplay(resultado.toString());
+}
   return (
     <View style={styles.container}>
         <Text style={styles.display}>{display}</Text>
         
-        <Viewc style={styles.linha}>
-          <botao texto="7" onPress={() => adicionar("7")} />
-          <botao texto="8" onPress={() => adicionar("8")} />  
-          <botao texto="9" onPress={() => adicionar("9")} />
-          <botao texto="/" onPress={() => adicionar("/")} />  
+        <View style={styles.linha}>
+          <Botao texto="7" onPress={() => adicionar("7")} />
+          <Botao texto="8" onPress={() => adicionar("8")} />  
+          <Botao texto="9" onPress={() => adicionar("9")} />
+          <Botao texto="/" onPress={() => adicionar("/")} />  
         </View>
 
-        <Viewc style={styles.linha}>
-          <botao texto="4" onPress={() => adicionar("4")} />
-          <botao texto="5" onPress={() => adicionar("5")} />  
-          <botao texto="6" onPress={() => adicionar("6")} />
-          <botao texto="*" onPress={() => adicionar("*")} />  
+        <View style={styles.linha}>
+          <Botao texto="4" onPress={() => adicionar("4")} />
+          <Botao texto="5" onPress={() => adicionar("5")} />  
+          <Botao texto="6" onPress={() => adicionar("6")} />
+          <Botao texto="*" onPress={() => adicionar("*")} />  
         </View>
 
-        <Viewc style={styles.linha}>
-          <botao texto="1" onPress={() => adicionar("1")} />
-          <botao texto="2" onPress={() => adicionar("2")} />  
-          <botao texto="3" onPress={() => adicionar("3")} />
-          <botao texto="-" onPress={() => adicionar("-")} />  
+        <View style={styles.linha}>
+          <Botao texto="1" onPress={() => adicionar("1")} />
+          <Botao texto="2" onPress={() => adicionar("2")} />  
+          <Botao texto="3" onPress={() => adicionar("3")} />
+          <Botao texto="-" onPress={() => adicionar("-")} />  
         </View>
 
-        <Viewc style={styles.linha}>
-          <botao texto="0" onPress={() => adicionar("0")} />
-          <botao texto="C" onPress={limpar} />  
-          <botao texto="=" onPress={calcular} />
-          <botao texto="+" onPress={() => adicionar("+")} />  
+        <View style={styles.linha}>
+          <Botao texto="0" onPress={() => adicionar("0")} />
+          <Botao texto="C" onPress={limpar} />  
+          <Botao texto="=" onPress={calcular} />
+          <Botao texto="+" onPress={() => adicionar("+")} />  
         </View>
     </View>
 
   );
 }
-Function Botao({texto, onPress}) {
+function Botao({texto, onPress}) {
   return(
-    <TouchableOpacity style={StyleSheet.botao} onPress={onPress}>
-      <Text style={StyleSheet.textoBotao}>{texto}</Text>
+    <TouchableOpacity style={styles.botao} onPress={onPress}>
+      <Text style={styles.textoBotao}>{texto}</Text>
     </TouchableOpacity>
   );
 }
